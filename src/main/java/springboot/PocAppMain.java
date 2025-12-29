9e15672532f25e5fdb4fddc2c00bd965534c691c
jfraser2 <69680391+jfraser2@users.noreply.github.com>
2025-12-28 21:15:53 -0600

    version that booted

--- src/main/java/springboot/PocAppMain.java
+++ src/main/java/springboot/PocAppMain.java
@@ -1,14 +1,13 @@
  package springboot;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 //import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.r2dbc.config.EnableR2dbcAuditing;
+import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /* @SpringBootApplicaton is the combination of
  *  @EnableAutoConfiguration, @ComponentScan and @Configuration annotations.
@@ -16,6 +15,9 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EntityScan(basePackages = {"springboot.entities"})
+@EnableR2dbcRepositories(basePackages = "springboot.repositories")
+@EnableTransactionManagement
+@EnableR2dbcAuditing // Optional: if you use R2DBC auditing features
 @ComponentScan(basePackages = {"springboot", "springboot.configurations"})
 public class PocAppMain    // Proof of Concept App Main
 {
